d66b6ce2f507f463faaa6567d7fc6a671864567c
alexandre dnk <101813042+dagarbolino@users.noreply.github.com>
2023-09-28 13:55:41 +0200

    commit pour mentoring

--- assets/script.js
+++ assets/script.js
@@ -19,37 +19,93 @@ const slides = [
 
 let flecheGauche = document.querySelector(".arrow_left")
 flecheGauche.addEventListener("click", () => {
+
+
+	//console.log(slides[i].image)
 	console.log("vous avez cliqué sur la fleche de gauche")
 })
 
+
+
+let val = slides.values();
+
+for (let i of val) {
+  console.log(i);
+}
+
+listData = 0
+for (let i = 0; i < slides.length; i++) {
+	let listData = slides[i]
+
+	console.log(listData)
+}
+
+listImg = 0
+for(let i = 0; i < slides.length; i++) {
+	let listImg = slides.length[i]
+	console.log(listImg)
+}
+
 let flecheDroite = document.querySelector(".arrow_right")
 flecheDroite.addEventListener("click", () => {
-	
+
 	console.log("vous avez cliqué sur la fleche de droite")
 })
 
 
 
 
-
-
-
- function changeImg(sens) {
-  
+function changeImg(sens) {
 
 	let number = 0;
 	number = number + sens;
-	if (number > 2)
-		number = 0;
+	if (number > 0)
+		number = + 1
 	if (number < 0)
-		number = 2;
+		number = + 1
 
-document.getElementById("slider").src = "./assets/images/slideshow/" + slides[number].image;
+
+	document.getElementById("slider").src = "./assets/images/slideshow/" + slides[number].image;
 	console.log(slides[number].image)
 
+}
 
 //console.log(slides[i].image)
+
+
+
+
+
+
+
+/**
+ * 
+let sliderImg = document.getElementById("slider")
+let image
+let i = 0
+
+function left() {
+	if(i <= 0) i = slides.length.image;
+	i--;
+	return setImg() 
 }
+
+function right () {
+	if(i >= slides.length.image - 1) i = -1
+	i++
+	return setImg()
+}
+
+function setImg() {
+	return sliderImg.setAttribute('src', "./assets/images/slideshow/" + setImg[i].image)
+}
+
+console.log(slides[i].image)
+
+ */
+
+
+
 
 
 
