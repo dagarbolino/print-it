e8b1cf30f18bf65483c5ca97753fab7733e5aaaf
alexandre dnk <101813042+dagarbolino@users.noreply.github.com>
2023-10-02 22:08:12 +0200

    commit pour image et tagline ok

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+let currentImageIndex = 0;
+
+let currentTagLineIndex = 0;
+
 const slides = [
 	{
 		"image": "slide1.jpg",
@@ -18,272 +22,60 @@ const slides = [
 ]
 
 
+const bannerContainer = document.getElementById('banner');
 
+const imageElement = document.querySelector('.banner-img');
 
-
+// Permet d'avoir un tableau: image
 let images = slides.map(slides => slides.image);
-let num = 0
-let mainImg = document.querySelector(".banner-img").src =
-	"./assets/images/slideshow/" + images[num];
 
+// Permet d'avoir un tableau: tagLine
+let tagLines = slides.map(slides => slides.tagLine);
+//console.log(tagLines)
 
-
-function next() {
-	num++
-	if (num >= images.length) {
-		num =
-			mainImg = mainImg + images[num]
-	} else {
-		mainImg = mainImg + images[num]
-	}
-	return next
+function showImage(index) {
+	imageElement.src = "./assets/images/slideshow/" + images[index];
 }
 
-function back() {
-
-}
-
-function dottt() {
-	let dottt = images.length
-	console.log(dottt)
-}
-
-let buletPoint = document.querySelector(".dot")
-
-
-
-let flecheDroite = document.querySelector(".arrow_right")
-flecheDroite.addEventListener("click", (back) => {
-	
-	//console.log(event.target)
-})
-
-
-
-
-let flecheGauche = document.querySelector(".arrow_left")
-flecheGauche.addEventListener("click", (next) => {
-
-	//console.log(event.target)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("slider").src = "./assets/images/slideshow/" + slides[number].image;
-
-
-
-	//console.log(event.currentTarget)//console.log(slides)//console.log(slides[0])//console.log(slides[0].image)
-
-
-
-	/**
-	 * 	for (let i = 0; i < images.length; i++) {
-			console.log(images[i])
-		}
-	 */
-
-	/**
-	 * 	for (let i = 0; i < slides.length; i++) {
-			//listImg = slides.length
-			//console.log(listeImag)  4
-	
-			listeImag = slides[i].image
-			console.log(listeImag)
-		}
-	
-		//let tabb = Object.values(slides)
-		//console.log(tabb)
-	
-	 */
-
-	//console.log(slides[0].image)
-	//console.log(slides[0].tagLine)
-	//console.log("vous avez cliqué sur la fleche de gauche")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * 
-function changeImg(sens) {
-
-	let elementJpg = 0;
-	elementJpg = elementJpg + sens ;
-	if (elementJpg > 0)
-		elementJpg = +1
-	if (elementJpg < 0)
-		elementJpg = +1
-
-
-	document.getElementById("slider").src = "./assets/images/slideshow/" + slides[elementJpg].image;
-	console.log(slides[elementJpg].image)
-
- }
-
-
-
-
-
-
-
-
-
-
- listData = 0
-	for (let i = 0; i < slides.image; i++) {
-		let listData = slides[i]
-	
-		console.log(listData)
-		//return listData 
-	}
- */
-
-
-
-/**
- * 	
-
-	const iterator1 = slides.entries()
-	console.log(iterator1.next().value) 
- 
- */
-
-/*
-	
-
-	//slides.forEach((element) => console.log(element))
-
-	for (let element of slides) {
-		element = slides.image
-		console.log(slides)
-
-	}*/
-
-
-/*** function changeImg(sens) {
-
-	let number = 0;
-	number = number + sens ++;
-	if (number ++ > 0)
-		number = + 1
-	if (number ++ < 0)
-		number = + 1
-
-
-	document.getElementById("slider").src = "./assets/images/slideshow/" + slides[number].image;
-	console.log(slides[number].image)
- */
-
-
-/**
- * 
-listData = 0
-for (let i = 0; i < slides.length; i++) {
-	let listData = slides[i]
-
-	console.log(listData)
+function showTagline(index) {
+  bannerContainer.querySelector('p').innerHTML = tagLines[index];
 }
 
 
-
-
-listImg = 0
-for(let i = 0; i < slides.length; i++) {
-	let listImg = slides.length[i]
-	console.log(listImg)
-}
- */
-
-
-
-/*
-function changeImg(sens) {
-
-	let number = 0;
-	number = number + sens ++;
-	if (number ++ > 0)
-		number = + 1
-	if (number ++ < 0)
-		number = + 1
-
-
-	document.getElementById("slider")
-	.src = "./assets/images/slideshow/" + slides[number].image;
-	console.log(slides[number].image)
-
+function prevImage() {
+	currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+	showImage(currentImageIndex);
 }
 
-//console.log(slides[i].image)
-
-*/
-
-
-/**
- * 
-let sliderImg = document.getElementById("slider")
-let image
-let i = 0
-
-function left() {
-	if(i <= 0) i = slides.length.image;
-	i--;
-	return setImg() 
+function nextImage() {
+	currentImageIndex = (currentImageIndex + 1) % images.length;
+	showImage(currentImageIndex);
 }
 
-function right () {
-	if(i >= slides.length.image - 1) i = -1
-	i++
-	return setImg()
+
+function prevTagline() {
+  currentTagLineIndex = (currentTagLineIndex - 1 + tagLines.length) % tagLines.length;
+  showTagline(currentTagLineIndex);
 }
 
-function setImg() {
-	return sliderImg.setAttribute('src', "./assets/images/slideshow/" + setImg[i].image)
+function nextTagline() {
+  currentTagLineIndex = (currentTagLineIndex + 1) % tagLines.length;
+  showTagline(currentTagLineIndex);
 }
 
-console.log(slides[i].image)
+// Initialisation
+showImage(currentImageIndex);
+showTagline(currentTagLineIndex);
 
- */
+// Écouteurs d'événements pour les flèches
+
+
+document.querySelector('.arrow_left').addEventListener('click', function() {
+  prevImage();
+  prevTagline();
+});
+
+document.querySelector('.arrow_right').addEventListener('click', function() {
+  nextImage();
+  nextTagline();
+});
